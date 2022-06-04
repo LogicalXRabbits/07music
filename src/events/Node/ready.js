7646ae4d92cc29d40d2784a625d3b2085e2f6be6
LogicalXRabbits <99948517+LogicalXRabbits@users.noreply.github.com>
2022-06-04 18:50:21 +0800

    mengubah bahasa client logger menjadi indo

--- src/events/Node/ready.js
+++ src/events/Node/ready.js
@@ -4,7 +4,7 @@ module.exports = {
     name: "ready",
     run: async (client, name) => {
         client.logger.log(`Lavalink "${name}" connected.`, "ready");
-        client.logger.log("Auto Reconnect Collecting player 24/7 data", "log");
+        client.logger.log("Auto Reconnect Dan Mengumpulkan data user 24/7", "log");
         const maindata = await db.find()
         client.logger.log(`Auto Reconnect found ${maindata.length ? `${maindata.length} queue${maindata.length > 1 ? 's' : ''}. Resuming all auto reconnect queue` : '0 queue'}`, "ready");
         for (let data of maindata) {
