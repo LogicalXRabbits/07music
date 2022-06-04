a1005081a9a6ca856a1db735247acc785502b41a
LogicalXRabbits <99948517+LogicalXRabbits@users.noreply.github.com>
2022-06-04 19:13:15 +0800

    memasukan semua bot kelas ke monitoring uptimerobot

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const port = 3000
 
-app.get('/', (req, res) => res.send('bot aktif'))
+app.get('/', (req, res) => res.send('bot aktif!!!'))
 
 app.listen(port, () =>
 console.log(`Your app is listening a http://localhost:${port}`)
