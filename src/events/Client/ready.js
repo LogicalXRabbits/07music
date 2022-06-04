786caf581d44a73d62d6b84d44c9cce6c6a89e22
LogicalXRabbits <99948517+LogicalXRabbits@users.noreply.github.com>
2022-06-04 18:44:41 +0800

    Bot 07
    mengupdate event dan client pada bot

--- src/events/Client/ready.js
+++ src/events/Client/ready.js
@@ -6,7 +6,7 @@ run: async (client) => {
     client.logger.log(`${client.user.username} online!`, "ready");
     client.logger.log(`Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users`, "ready");
    
-    let statuses = ['/help', `Prefix ${prefix}`];
+    let statuses = ['/help', `OFFICIAL 07 BOT MUSIC`];
     setInterval(function() {
   	let status = statuses[Math.floor(Math.random()*statuses.length)];		
         client.user.setPresence({
